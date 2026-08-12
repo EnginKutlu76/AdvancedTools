88927aa6463b97367ce0851890efa89523a2f84e
Engin Kutlu <116170963+EnginKutlu76@users.noreply.github.com>
2026-08-12 18:47:16 +0300

    Add: Climb stairs problem solution

---
+++ Assets/AdvanceScripts/ClimbingStairs.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class ClimbingStairs : MonoBehaviour
+{
+/*    class Solution
+    {
+        public:
+    int climbStairs(int n)
+        {
+            if (n < 1) return 0;
+
+            int prev1 = 1;
+            int prev2 = 2;
+            for (int i = 3; i <= n; i++)
+            {
+                int tmp = prev2;
+                prev2 += prev1;
+                prev1 = tmp;
+            }
+            return prev2;
+        }
+    };
+*/
+}
