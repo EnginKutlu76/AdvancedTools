2c655c363c1da548b254c8471598d93d3f402f8b
Engin Kutlu <116170963+EnginKutlu76@users.noreply.github.com>
2026-08-12 16:54:32 +0300

    Add: BestProfit problem solution

---
+++ Assets/AdvanceScripts/BestProfitSolution.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+using System.Numerics;
+using UnityEngine;
+
+public class BestProfitSolution : MonoBehaviour
+{
+/*    class Solution
+    {
+        public:
+    int maxProfit(vector<int>& prices)
+        {
+            if (prices.empty()) return 0;
+            int minPrice = prices[0];
+            int maxProfit = 0;
+
+            for (int price : prices)
+            {
+                minPrice = min(minPrice, price);
+                maxProfit = max(maxProfit, price - minPrice);
+            }
+            return maxProfit;
+        }
+    };
+*/
+}
