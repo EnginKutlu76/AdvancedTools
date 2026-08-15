2d24b6719e92e98e896b234d000b76c651311c8c
Engin Kutlu <116170963+EnginKutlu76@users.noreply.github.com>
2026-08-15 21:05:05 +0300

    Add: New problem solutions

---
+++ Assets/AdvanceScripts/ProblemSolutions/TwoSumSolution.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwoSumSolution : MonoBehaviour
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> numMap = new Dictionary<int, int>(); // A mapping to store numbers and their indices
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int complement = target - nums[i]; // Find the required number to reach the target
+                if (numMap.ContainsKey(complement))
+                {
+                    return new int[] { numMap[complement], i }; // Return indices of the complement and current number
+                }
+                numMap[nums[i]] = i; // Store the number with its index
+            }
+            return new int[] { }; // This line is never reached due to the problem guarantee
+        }
+    }
+}
